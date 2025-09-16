f4be444fc201b649a704d145eca8c962c1656ea1
indextrown <indextrown@gmail.com>
2025-09-16 13:28:43 +0900

    Initial Commit

---
+++ Pindora/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pindora
+//
+//  Created by 김동현 on 9/16/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
